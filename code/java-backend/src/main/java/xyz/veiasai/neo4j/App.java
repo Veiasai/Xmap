43b41a43f3853e007344ffb4297650c2fb962bbc
DESKTOP-LGNPT8L\Veiasai <917971502@qq.com>
2018-07-30 10:31:18 +0800

    更新部署

--- code/java-backend/src/main/java/xyz/veiasai/neo4j/App.java
+++ code/java-backend/src/main/java/xyz/veiasai/neo4j/App.java
@@ -3,9 +3,11 @@ package xyz.veiasai.neo4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.MultipartConfigElement;
 
--- code/wepy-frontend/src/properties/Const.js
+++ code/wepy-frontend/src/properties/Const.js
@@ -1,4 +1,4 @@
-var httpHead = "http://202.120.40.109:23385";
+var httpHead = "http://202.120.40.109:23384";
 var imgHead = "http://p9hefrt2w.bkt.clouddn.com/";
 
 export  {httpHead, imgHead}
