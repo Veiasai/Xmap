357abe653995d0799ce7b19e09b813ec5eb273fa
veia-mac <917971502@qq.com>
2018-09-07 21:22:31 +0800

    更新文档

--- code/react-frontend/src/UserData.js
+++ code/react-frontend/src/UserData.js
@@ -9,16 +9,13 @@ class UserData {
     init = false;
 
     @observable
-    isLogin = true;
+    isLogin = false;
     @observable
-    userID = 'oEzSZ5RA3PFN5Ho0J0Bz0i66Gp9k';
+    userID = "";
     @observable
     buildingList = [];
     @observable
-    currentBuilding = {
-        id: 'ba974511-df18-4b7d-a8e0-9257b79b2bb8',
-        name: '交通大学闵行校区-软件学院',
-    };
+    currentBuilding = {};
     @observable
     currentPathList = [];
     @observable
